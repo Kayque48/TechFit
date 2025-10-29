bfff632fa0ed7d57cc5b8f42cbf23261ce4155d7
Kayque <kayquec158@gmail.com>
2025-10-29 16:47:12 -0300

    chore: reorganizando estrutura do projeto e padronizando pastas

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inícial</title>
+</head>
+<body>
+
+    <div>
+        <h1>Página de Cadastro do Usuário</h1>
+        <a href="public/cadUser.html">Aqui</a>
+    </div>
+
+    <div>
+        <h1>Página de Login do Usuário</h1>
+        <a href="public/loginUser.html">Aqui</a>
+    </div>
+
+    <div>
+        <h1>Página de Cadastro de Produto</h1>
+        <a href="public/cadastroProduto.html">Aqui</a>
+    </div>
+
+    <div>
+        <h1>Página da Tela do Cliente</h1>
+        <a href="public/telaCliente.html">Aqui</a>
+    </div>
+
+
+    
+</body>
+</html>
