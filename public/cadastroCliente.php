c9fbdc9820ece510abb3011c6a1f00423e4bef62
Kayque <kayquec158@gmail.com>
2025-11-26 13:58:32 -0300

    chore: tentando integrar o front de cadastro com o back

--- public/cadastroCliente.php
+++ public/cadastroCliente.php
@@ -1,3 +1,13 @@
+<?php
+    
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    require_once __DIR__ . '/../src/controllers/AlunoIntegracao.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -24,79 +34,58 @@
   <main class="container mt-4">
     <div class="row g-3">
 
-      <!-- Nome -->
-      <div class="col-sm-6">
-        <label for="firstName" class="form-label">Primeiro nome</label>
-        <input type="text" class="form-control" id="firstName" required>
-        <div class="invalid-feedback">Valid first name is required.</div>
-      </div>
 
-      <!-- Sobrenome -->
-      <div class="col-sm-6">
-        <label for="lastName" class="form-label">Sobrenome</label>
-        <input type="text" class="form-control" id="lastName" required>
-        <div class="invalid-feedback">Sobrenome</div>
-      </div>
-
-      <!-- Usuário -->
-      <div class="col-12">
-        <label for="username" class="form-label">Nome de usuário</label>
-        <div class="input-group has-validation">
-          <!-- <span class="input-group-text">@</span> -->
-          <input type="text" class="form-control" id="username" placeholder="" required>
-          <div class="invalid-feedback">Your username is required.</div>
+      <form action="" method="post">
+        <input type="hidden" name="cadastro" value="criar">
+        <!-- Nome -->
+        <div class="col-sm-6">
+          <label for="firstName" class="form-label">Primeiro nome</label>
+          <input type="text" class="form-control" id="name" required>
+          <div class="invalid-feedback">Valid first name is required.</div>
         </div>
+
+        <!-- Email -->
+        <div class="col-12">
+          <label for="email" class="form-label">
+            Email <span class="text-body-secondary"></span>
+          </label>
+          <input type="email" class="form-control" id="email" placeholder="">
+          <div class="invalid-feedback">Please enter a valid email address for shipping updates.</div>
+        </div>
+
+        <!-- Endereço -->
+        <div class="col-12">
+          <label for="address" class="form-label">Endereço</label>
+          <input type="text" class="form-control" id="endereco" placeholder="" required>
+          <div class="invalid-feedback">Please enter your shipping address.</div>
+        </div>
+
+        <!-- Plano -->
+      <div class="col-md-4">
+      <label for="state" class="form-label">Assinatura</label>
+      <select class="form-select" id="state" required>
+          <option value="">Selecione seu plano</option>
+          <option value="mensal">Plano Mensal - R$ 99,90</option>
+          <option value="trimestral">Plano Trimestral - R$ 269,90 (economia de 10%)</option>
+          <option value="semestral">Plano Semestral - R$ 499,90 (economia de 15%)</option>
+          <option value="anual">Plano Anual - R$ 899,90 (economia de 25%)</option>
+          <option value="vip">Plano VIP - R$ 1.299,90 (acesso ilimitado + personal trainer)</option>
+      </select>
+      <div class="invalid-feedback">Por favor, selecione um plano válido.</div>
       </div>
 
-      <!-- Email -->
-      <div class="col-12">
-        <label for="email" class="form-label">
-          Email <span class="text-body-secondary"></span>
-        </label>
-        <input type="email" class="form-control" id="email" placeholder="">
-        <div class="invalid-feedback">Please enter a valid email address for shipping updates.</div>
-      </div>
 
-      <!-- Endereço -->
-      <div class="col-12">
-        <label for="address" class="form-label">Endereço</label>
-        <input type="text" class="form-control" id="address" placeholder="" required>
-        <div class="invalid-feedback">Please enter your shipping address.</div>
-      </div>
-
-      <!-- Endereço 2 -->
-      <div class="col-12">
-        <label for="address2" class="form-label">
-          Telefone <span class="text-body-secondary"></span>
-        </label>
-        <input type="text" class="form-control" id="address2" placeholder="">
-      </div>
-
-      <!-- Plano -->
-    <div class="col-md-4">
-    <label for="state" class="form-label">Assinatura</label>
-    <select class="form-select" id="state" required>
-        <option value="">Selecione seu plano</option>
-        <option value="mensal">Plano Mensal - R$ 99,90</option>
-        <option value="trimestral">Plano Trimestral - R$ 269,90 (economia de 10%)</option>
-        <option value="semestral">Plano Semestral - R$ 499,90 (economia de 15%)</option>
-        <option value="anual">Plano Anual - R$ 899,90 (economia de 25%)</option>
-        <option value="vip">Plano VIP - R$ 1.299,90 (acesso ilimitado + personal trainer)</option>
-    </select>
-    <div class="invalid-feedback">Por favor, selecione um plano válido.</div>
-    </div>
-
-
-      <!-- CEP -->
-      <div class="col-md-3">
-        <label for="zip" class="form-label">CEP</label>
-        <input type="text" class="form-control" id="zip" required>
-        <div class="invalid-feedback">Zip code required.</div>
-      </div>
+        <!-- CEP -->
+        <div class="col-md-3">
+          <label for="zip" class="form-label">CEP</label>
+          <input type="text" class="form-control" id="zip" required>
+          <div class="invalid-feedback">Zip code required.</div>
+        </div>
+      </form>
 
       <div>
         <ul>
-        <li>já tem uma conta? <a href="loginUser.html">Cadastro</a></li>
+        <li>já tem uma conta? <a href="loginCliente.php">Logar</a></li>
       </ul>
       </div>
 
